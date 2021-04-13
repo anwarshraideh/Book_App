2f1e0b2260b5a67f0568143821fb407f11f6a4be
anwarshraideh <ahshraida@gmail.com>
2021-04-13 22:57:21 +0300

    lab12

---
+++ data/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO BookList VALUES(
+    1,
+    'The Cossacks',
+    'Leo Nikolayevich Tolstoy',
+    'ISBN_13 9798640765274',
+    'https://books.google.com/books/content?id=LeY9JcYa3EsC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+    'There is no Description for this book'
+);
